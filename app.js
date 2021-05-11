28395a979e0894fb3104776c264450a6ff2ea950
Mitchell Debrah <debrahmitch@Mitchells-MacBook-Pro.local>
2021-05-11 10:21:14 -0400

    Created Game interface

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ const boardDkCreation = () => {
         values = 10;
       if (ranks[boardDkcard] == "A") values = 11;
       let card = {
-        ranks: ranks[boardDkcard],
+        Rank: ranks[boardDkcard],
         Suit: cardSuits[suits],
-        Values: values,
+        Value: values,
       };
       boardDk.push(card);
     }
@@ -84,98 +84,3 @@ const userInterface = () =>{
     }
 
 }
-// got inspiration from a popular card shuffle code
-const cardShuffle = () =>{
-    for (let i = 0; i < 200; i++){
-            
-        let placement0 = Math.floor((Math.random() * boardDk.length));
-        let placement1 = Math.floor((Math.random() * boardDk.length));
-        let spot = boardDk[placement0];
-
-        boardDk[placement0] = boardDk[placement1];
-        boardDk[placement1] = spot;
-    }
-}
-
-const beginBJGame = ()=>{
-    document.querySelector('reset').value = 'Reset';
-    document.querySelector("status").style.display="none";
-    boardDkCreation();
-    playersCreated(2);
-    cardShuffle();
-    userInterface();
-    document.querySelector('player_' + currentCompetitor).classList.add('active');
-    dealerhand();
-
-    currentCompetitor = 0;
-
-
-}
-
-const dealerhand = ()=>{ // distributes the cards to the players each player gets two cards.
-    for(let j = 0; j < 2; j++){
-        for(let comp of competitors){
-            let card = boardDk.pop();
-            competitors[comp].Hand.push(card);
-            displayCard(card, comp);
-            newScores();
-
-        }
-    }
-
-    newDeck();
-
-
-}
-
-
-const displayCard = (card, player)=>{
-    let hd = document.getElementById('hand_' + player);
-    hd.appendChild(cardInterface(card));
-
-}
-
-const cardInterface = (card) =>{
-    let intface = document.createElement('div');
-    let symbol = '';
-    if (card.Suit == 'H')
-    symbol = '&hearts;'
-    else if (card.Suit == 'S')
-    symbol = '&spades;'
-    else if(card.Suit == 'D')
-    symbol = '&diamonds'
-    else 
-    symbol ='&clubs;'
-
-
-    intface .className = 'card';
-    intface .innerHTML = card.Value + '<br/>' + symbol;
-    return intface;
-
-    
-
-
-    
-    
-    
-    
-    
-
-    
-}
-
-const newScores = (player) =>{ // returns the number of points at hand
-    let scores = 0;
-    for(let pts of competitors[player].Hand.length){
-
-    }
-
-
-    
-}
-    
-const newDeck = () =>{
-    
-
-}
-
